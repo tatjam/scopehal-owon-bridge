65e4187a22a5b548dc46bac2784708ce22bff9e8
tatjam <tatjam02@gmail.com>
2023-12-19 22:51:04 +0100

    Calibration read and safety checks

--- src/vds1022/Driver.cpp
+++ src/vds1022/Driver.cpp
@@ -1,10 +1,11 @@
 #include "Driver.h"
 #include "../../lib/log/log.h"
+#include <string>
 
 // Little-endian! LSB at littlest address
 
 template<typename T>
-CommandResponse Driver::send_command(uint32_t addr, T data)
+void Driver::send_command_raw(uint32_t addr, T data)
 {
 	std::array<uint8_t, sizeof(T) + 4 + 1> bytes{};
 	bytes[0] = addr & 0xFF;
@@ -13,7 +14,7 @@ CommandResponse Driver::send_command(uint32_t addr, T data)
 	bytes[3] = (addr & 0xFF000000) >> (8 * 3);
 	bytes[4] = sizeof(T);
 	// write data
-	for(int i = 0; i < sizeof(T); i++)
+	for(size_t i = 0; i < sizeof(T); i++)
 	{
 		bytes[5 + i] = (data >> (8 * i)) & 0xFF;
 	}
@@ -21,6 +22,12 @@ CommandResponse Driver::send_command(uint32_t addr, T data)
 	// the timeouts and message division that libusb may do
 	libusb_bulk_transfer(hnd, write_ep, bytes.data(), bytes.size(), nullptr, 0);
 
+}
+
+template<typename T>
+CommandResponse Driver::send_command(uint32_t addr, T data)
+{
+	send_command_raw<T>(addr, data);
 	// Now listen for response
 	std::array<uint8_t, 5> read_bytes{};
 	libusb_bulk_transfer(hnd, read_ep, read_bytes.data(), read_bytes.size(), nullptr, 0);
@@ -35,6 +42,8 @@ CommandResponse Driver::send_command(uint32_t addr, T data)
 	return out;
 }
 
+
+
 bool Driver::init(libusb_device_handle* _hnd, uint8_t _write_ep, uint8_t _read_ep)
 {
 	this->hnd = _hnd;
@@ -42,7 +51,7 @@ bool Driver::init(libusb_device_handle* _hnd, uint8_t _write_ep, uint8_t _read_e
 	this->read_ep = _read_ep;
 
 	// Check correct device version to avoid possible damage
-	auto rsp = send_command(0x4001, 86);
+	auto rsp = send_command<uint8_t>(0x4001, 86);
 	if(rsp.value != 1)
 	{
 		LogError("Incorrect device version\n");
@@ -51,7 +60,108 @@ bool Driver::init(libusb_device_handle* _hnd, uint8_t _write_ep, uint8_t _read_e
 
 	LogNotice("Correct device version detected\n");
 
+	if(!read_flash())
+	{
+		return false;
+	}
+
+	// Check FPGA, we don't flash it! The user must atleast launch
+	// the official software once
+	rsp = send_command<uint8_t>(0X0223, 0);
+	if(rsp.value == 0)
+	{
+		// TODO: Flash the device (kind of wonky licensing, so maybe not a good idea!)
+		LogError("Device is not flashed. Launch official software to do so!\n");
+		LogError("Once software has been launched once, the device will remain\\
+flashed until it's disconnected from the computer. (Software can be closed)\n");
+		LogError("If you are running linux, you can use: https://github.com/florentbr/OWON-VDS1022/tree/master\n");
+		return false;
+	}
 
 	return true;
 }
+
+bool Driver::read_flash()
+{
+	// Read flash command expects 1 byte argument
+	send_command_raw<uint8_t>(0x01b0, 1);
+	std::array<uint8_t, 2002> flash{};
+	libusb_bulk_transfer(hnd, read_ep, flash.data(), flash.size(), nullptr, 0);
+
+	// Check header
+	if(!(
+			(flash[0] == 0xAA && flash[1]  == 0x55) ||  // Header 0x55AA
+			(flash[0] == 0x55 && flash[1] == 0xAA)))	// Header 0xAA55
+	{
+		LogError("Invalid flash header version, aborting\n");
+		return false;
+	}
+
+	// Check version
+	if(flash[2] != 2)
+	{
+		LogError("Invalid flash version, aborting\n");
+		return false;
+	}
+
+	// Read calibration
+	for(int ch = 0; ch < 2; ch++)
+	{
+		for(int volt = 0; volt < 10; volt++)
+		{
+			// Gain
+			uint8_t gain0 = flash[6 + ch * 20 + volt * 2 + 0];
+			uint8_t gain1 = flash[6 + ch * 20 + volt * 2 + 1];
+			uint8_t ampl0 = flash[46 + ch * 20 + volt * 2 + 0];
+			uint8_t ampl1 = flash[46 + ch * 20 + volt * 2 + 0];
+			uint8_t comp0 = flash[86 + ch * 20 + volt * 2 + 0];
+			uint8_t comp1 = flash[86 + ch * 20 + volt * 2 + 1];
+
+			uint16_t gain = (uint16_t)gain0 | ((uint16_t)gain1 << 8);
+			uint16_t ampl = (uint16_t)ampl0 | ((uint16_t)ampl1 << 8);
+			uint16_t comp = (uint16_t)comp0 | ((uint16_t)comp1 << 8);
+
+			calibration[ch].gain[volt] = gain;
+			calibration[ch].ampl[volt] = ampl;
+			calibration[ch].comp[volt] = comp;
+		}
+	}
+
+	// Read version to determine implementation quirks
+	// (It's a nul terminated C string)
+	std::string ver;
+	uint8_t* ptr = &flash[207];
+	do
+	{
+		ver.push_back(*ptr);
+	}
+	while(*(ptr++) != 0);
+
+
+	if(ver.size() > 3)
+	{
+		int vern = ((int)ver[1] - 48);
+		if(vern > 2 && vern <= 9)
+		{
+			old_board = false;
+		}
+		else if(ver.rfind("V2.7.0"))
+		{
+			old_board = false;
+		}
+		else
+		{
+			old_board = true;
+		}
+	}
+	else
+	{
+		LogError("Bad firmware version: %s", ver.c_str());
+		return false;
+	}
+
+
+	return true;
+}
+
 
--- src/vds1022/Driver.h
+++ src/vds1022/Driver.h
@@ -12,19 +12,42 @@ struct CommandResponse
 	uint32_t value;
 };
 
+// 5mV 10mV 20mV 50mV 100mV 200mV 500mV 1V 2V 5V
+struct Calibration
+{
+	std::array<uint16_t, 10> gain;
+	std::array<uint16_t, 10> ampl;
+	std::array<uint16_t, 10> comp;
+};
+
 class Driver
 {
 private:
+
+	bool old_board;
 
 	libusb_device_handle* hnd;
 	uint8_t write_ep;
 	uint8_t read_ep;
 
+	Calibration calibration[2];
+
 	// T may be uint8_t, uint16_t or uint32_t
+	// BEWARE! The type of T is vital for the command success!
 	template<typename T>
 	CommandResponse send_command(uint32_t addr, T data);
+
+	// Sends command but doesn't wait for reply, for non standard commands
+	template<typename T>
+	void send_command_raw(uint32_t addr, T data);
+
+	// Obtains current oscilloscope calibration, and returns
+	// if everything is safe to use
+	bool read_flash();
+
 public:
 
 	bool init(libusb_device_handle* _hnd, uint8_t _write_ep, uint8_t _read_ep);
 
 };
+
