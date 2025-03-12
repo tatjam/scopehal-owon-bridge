20db1e2b8bc32a64835fc5c8d3f1240c6d12bb0d
tatjam <tatjam02@gmail.com>
2025-03-12 11:27:42 +0100

    Start SCPI server

--- src/vds1022/OWONSCPIServer.h
+++ src/vds1022/OWONSCPIServer.h
@@ -1,6 +1,38 @@
 #pragma once
 
-class OWONSCPIServer
-{
+#include "../../lib/scpi-server-tools/BridgeSCPIServer.h"
 
+class OWONSCPIServer : public BridgeSCPIServer
+{
+public:
+	OWONSCPIServer(ZSOCKET sock);
+	virtual ~OWONSCPIServer();
+
+protected:
+	std::string GetMake() override;
+	std::string GetModel() override;
+	std::string GetSerial() override;
+	std::string GetFirmwareVersion() override;
+	size_t GetAnalogChannelCount() override;
+	std::vector<size_t> GetSampleRates() override;
+	std::vector<size_t> GetSampleDepths() override;
+	void AcquisitionStart(bool oneShot) override;
+	void AcquisitionForceTrigger() override;
+	void AcquisitionStop() override;
+	bool IsTriggerArmed() override;
+	void SetChannelEnabled(size_t chIndex, bool enabled) override;
+	void SetAnalogCoupling(size_t chIndex, const std::string& coupling) override;
+	void SetAnalogRange(size_t chIndex, double range_V) override;
+	void SetAnalogOffset(size_t chIndex, double offset_V) override;
+	void SetDigitalThreshold(size_t chIndex, double threshold_V) override;
+	void SetDigitalHysteresis(size_t chIndex, double hysteresis) override;
+	void SetSampleRate(uint64_t rate_hz) override;
+	void SetSampleDepth(uint64_t depth) override;
+	void SetTriggerDelay(uint64_t delay_fs) override;
+	void SetTriggerSource(size_t chIndex) override;
+	void SetTriggerLevel(double level_V) override;
+	void SetTriggerTypeEdge() override;
+	void SetEdgeTriggerEdge(const std::string& edge) override;
+	bool GetChannelID(const std::string& subject, size_t& id_out) override;
+	ChannelType GetChannelType(size_t channel) override;
 };
